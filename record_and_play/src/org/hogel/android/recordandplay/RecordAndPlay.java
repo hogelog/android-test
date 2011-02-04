8ce2f8562c70e22fdd8e13b8bfac84982b68931e
hogelog <konbu.komuro@gmail.com>
2011-02-05 04:39:41 +0900

    update record_and_play (fix).

--- record_and_play/src/org/hogel/android/recordandplay/RecordAndPlay.java
+++ record_and_play/src/org/hogel/android/recordandplay/RecordAndPlay.java
@@ -3,16 +3,35 @@ package org.hogel.android.recordandplay;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class RecordAndPlay extends Activity {
 	private final MediaRecorder recorder = new MediaRecorder();
 	private final MediaPlayer player = new MediaPlayer();
 	private boolean onRecord = false;
+	private boolean onPlay = false;
+	private String path = "/sdcard/recordandplay.3gp";
+
+	private void message(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+	}
+
+	private void setAppearance() {
+		final Button startRecord = (Button) findViewById(R.id.startRecordButton);
+		final Button stopRecord = (Button) findViewById(R.id.stopRecordButton);
+		final Button startPlay = (Button) findViewById(R.id.startPlayButton);
+		final Button stopPlay = (Button) findViewById(R.id.stopPlayButton);
+		startRecord.setEnabled(!onRecord && !onPlay);
+		stopRecord.setEnabled(onRecord);
+		startPlay.setEnabled(!onRecord && !onPlay);
+		stopPlay.setEnabled(onPlay);
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -20,47 +39,61 @@ public class RecordAndPlay extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		final String path = "/sdcard/recordandplay.3gp";
-
-		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-		recorder.setOutputFile(path);
-		try {
-			recorder.prepare();
-			player.setDataSource(path);
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		setAppearance();
 	}
 
-	public void clickRecord(View v) {
-		if (!onRecord) {
-			recorder.start(); // Recording is now started
-			Toast.makeText(this, "Start Recording", Toast.LENGTH_SHORT).show();
-			onRecord = true;
-		}
-	}
-
-	public void clickStop(View v) {
-		recorder.stop();
-		recorder.reset(); // You can reuse the object by going back to
-		Toast.makeText(this, "Stop Recording", Toast.LENGTH_SHORT).show();
-		onRecord = false;
-	}
-
-	public void clickPlay(View v) {
-		if (!onRecord) {
+	public void clickStartRecorder(View v) {
+		if (!onRecord && !onPlay) {
 			try {
-				player.prepare();
-				player.start();
+				recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+				recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+				recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+				recorder.setOutputFile(path);
+				recorder.prepare();
+				message("Start Recording");
+				recorder.start(); // Recording is now started
+				onRecord = true;
 			} catch (IllegalStateException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+		setAppearance();
+	}
+
+	public void clickStopRecorder(View v) {
+		if (onRecord) {
+			recorder.stop();
+			message("Stop Recording");
+			onRecord = false;
+		}
+		setAppearance();
+	}
+
+	public void clickStartPlayer(View v) {
+		if (!onRecord && !onPlay) {
+			try {
+				player.setDataSource(path);
+				player.prepare();
+				message("Start Play");
+				player.start();
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			onPlay = true;
+		}
+		setAppearance();
+	}
+	public void clickStopPlayer(View v) {
+		if (onPlay) {
+			player.stop();
+			message("Stop Play");
+			player.reset();
+			onPlay = false;
+		}
+		setAppearance();
 	}
 }
