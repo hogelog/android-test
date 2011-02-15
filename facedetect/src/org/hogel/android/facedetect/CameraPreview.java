401f606371c1b74fcf70ecf6b2398d0248e0cbe2
hogelog <konbu.komuro@gmail.com>
2011-02-15 18:29:43 +0900

    FaceDetect: selectable camera size.

--- facedetect/src/org/hogel/android/facedetect/CameraPreview.java
+++ facedetect/src/org/hogel/android/facedetect/CameraPreview.java
@@ -10,52 +10,42 @@ import android.graphics.PointF;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.media.FaceDetector;
-import android.media.FaceDetector.Face;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback, PreviewCallback {
-    private SurfaceHolder holder;
-	private Camera camera;
-	private Bitmap bitmap;
-	private int[] rgbBuffer;
-	private byte[] yuvBuffer;
-	private YUV420toRGB8888 yuv2rgb;
-	private FaceDetector detector;
+    private final SurfaceHolder holder;
+	private final Bitmap bitmap;
+	private final int[] rgbBuffer;
+	private final byte[] yuvBuffer;
+	private final YUV420toRGB8888 yuv2rgb;
+	private final FaceDetector detector;
+	private final Camera camera;
 	private final FaceDetector.Face[] face = new FaceDetector.Face[1];
 	private final PointF point = new PointF();
 	private final Paint redPaint = new Paint();
 	private final CalcFPS fps = new CalcFPS();
 
-	private final int width;
-	private final int height;
-	
-	public CameraPreview(Context context, int width, int height) {
+	private final Camera.Size size;
+
+	public CameraPreview(Context context, Camera camera) {
 		super(context);
 
         holder = getHolder();
         holder.addCallback(this);
+
         redPaint.setColor(Color.RED);
         redPaint.setStyle(Paint.Style.STROKE);
-        this.width = width;
-        this.height = height;
-	}
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-	}
+        this.camera = camera;
 
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-        camera = Camera.open();
 		final Camera.Parameters params = camera.getParameters();
-		params.setPreviewSize(width, height);
-		camera.setParameters(params);
+		size = params.getPreviewSize();
 
-		final int w = width, h = height;
+        final int w = size.width, h = size.height;
+        Log.i("FaceDetect", w+"x"+h);
 
 		detector = new FaceDetector(w, h, 1);
 		rgbBuffer = new int[w * h];
@@ -65,17 +55,21 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         final int yuvPerBits = ImageFormat.getBitsPerPixel(params.getPreviewFormat());
 		yuvBuffer = new byte[w * h * yuvPerBits / 8];
-		camera.addCallbackBuffer(yuvBuffer);
-		camera.setPreviewCallbackWithBuffer(this);
+	}
 
-		camera.startPreview();
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		startPreview();
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
-    	camera.setPreviewCallback(null);
-    	camera.stopPreview();
-    	camera.release();
+    	//stopPreview();
 
     	Toast.makeText(getContext(), String.format("FPS Average: %.1f", fps.average), Toast.LENGTH_SHORT).show();
 	}
@@ -83,17 +77,17 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
 	@Override
 	public void onPreviewFrame(byte[] data, Camera camera) {
-		camera.setPreviewCallback(null);
+		stopPreview();
 
 		final Canvas canvas = holder.lockCanvas();
 
-		final int w = width, h = height;
+		final int w = size.width, h = size.height;
 		yuv2rgb.getFast(data, rgbBuffer);
 
 		canvas.drawBitmap(rgbBuffer, 0, w, 0, 0, w, h, false, null);
 		bitmap.setPixels(rgbBuffer, 0, w, 0, 0, w, h);
 		if (detector.findFaces(bitmap, face) == 1) {
-			Face f = face[0];
+			FaceDetector.Face f = face[0];
 			f.getMidPoint(point);
 			float x = point.x, y = point.y;
 			float fw = f.eyesDistance() * (float)0.7, fh = f.eyesDistance() * (float)1.2;
@@ -102,8 +96,19 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 		holder.unlockCanvasAndPost(canvas);
 
 		fps.calc();
-		Log.i("FaceDetector", String.format("FPS: %.1f, ave: %.1f", fps.current, fps.average));
+		Log.i("FaceDetect", String.format("FPS: %.1f, ave: %.1f", fps.current, fps.average));
 
-		camera.setPreviewCallback(this);
+		startPreview();
+	}
+
+	public void startPreview() {
+		camera.addCallbackBuffer(yuvBuffer);
+		camera.setPreviewCallbackWithBuffer(this);
+		camera.startPreview();
+	}
+
+	public void stopPreview() {
+		camera.setPreviewCallbackWithBuffer(null);
+		camera.stopPreview();
 	}
 }
--- facedetect/src/org/hogel/android/facedetect/FaceDetectActivity.java
+++ facedetect/src/org/hogel/android/facedetect/FaceDetectActivity.java
@@ -1,20 +1,115 @@
 package org.hogel.android.facedetect;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class FaceDetectActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	private static final String PREF_CAMERA_W = "CAM_W";
+	private static final String PREF_CAMERA_H = "CAM_H";
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	Camera camera;
+	CameraPreview preview;
+	SharedPreferences prefs;
+	List<Camera.Size> supportedSizes;
 
-        final CameraPreview preview = new CameraPreview(this, 480, 320);
-        setContentView(preview);
-    }
+	private void setCameraSize(int width, int height) {
+		final Camera.Parameters params = camera.getParameters();
+		params.setPreviewSize(width, height);
+		camera.setParameters(params);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+        camera = Camera.open();
+
+        final int cam_w = prefs.getInt(PREF_CAMERA_W, 0);
+        final int cam_h = prefs.getInt(PREF_CAMERA_H, 0);
+        if (cam_w != 0 && cam_h != 0) {
+        	setCameraSize(cam_w, cam_h);
+        }
+
+        preview = new CameraPreview(this, camera);
+		setContentView(preview);
+	}
+
+	@Override
+	protected void onPause() {
+		preview.stopPreview();
+		camera.release();
+		camera = null;
+		super.onPause();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		supportedSizes = camera.getParameters().getSupportedPreviewSizes();
+		final SubMenu sub = menu.addSubMenu(Menu.NONE, R.id.camera_sizes, Menu.NONE, "Camera Size");
+        final int cam_w = prefs.getInt(PREF_CAMERA_W, 0);
+        final int cam_h = prefs.getInt(PREF_CAMERA_H, 0);
+		for (int i = 0, len = supportedSizes.size(); i < len; ++i) {
+			Camera.Size size = supportedSizes.get(i);
+			String s = size.width + "x" + size.height;
+			MenuItem item = sub.add(R.id.camera_size_group, R.id.camera_size_item, i, s);
+			item.setCheckable(true);
+			if (size.width == cam_w && size.height == cam_h) {
+				item.setChecked(true);
+			}
+		}
+		sub.setGroupCheckable(R.id.camera_size_group, true, true);
+
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		preview.stopPreview();
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public void onOptionsMenuClosed(Menu menu) {
+		super.onOptionsMenuClosed(menu);
+		preview.startPreview();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.camera_size_item) {
+			item.setChecked(true);
+
+			final int order = item.getOrder();
+			final Camera.Size size = supportedSizes.get(order);
+			final Editor edit = prefs.edit();
+			edit.putInt(PREF_CAMERA_W, size.width);
+			edit.putInt(PREF_CAMERA_H, size.height);
+			edit.commit();
+
+			setCameraSize(size.width, size.height);
+			preview = new CameraPreview(this, camera);
+			setContentView(preview);
+		}
+		return true;
+	}
 }
